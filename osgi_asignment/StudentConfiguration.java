fd3af0a55e5f1e4ddacc745aedbb6ec977dafe48
prashant <prashant.dubey@tothenew.com>
2022-11-06 21:12:50 +0530

    saved work

---
+++ osgi_asignment/StudentConfiguration.java
@@ -0,0 +1,20 @@
+package com.mysite.core.services.impl;
+
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
+@ObjectClassDefinition(name = "Student Configuration",description="Configuration by Vivek")
+public @interface StudentConfiguration {
+
+
+	@AttributeDefinition(
+			name = "Number", required = true,
+			description = "Number of students allowed in the class")
+	        int no_of_students_allowed();
+
+
+	@AttributeDefinition(
+			name = "Marks", required = true,
+			description = "Passing marks required")
+	        double marks_required_to_pass();
+}
